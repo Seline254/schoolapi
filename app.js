69f5ebb4c9236985c969716523bc8514594b4d25
Seline254 <selineowino36@gmail.com>
2025-07-10 09:37:43 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// Entry file
+const express = require("express")
+const mongoose = require("mongoose")
+const cors= require("cors")
+require('dotenv').config()
+
+// middleware
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+// static files accessibility
+app.use("/uploads",express.static)
+
+
+// connection to the database
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("Mongo Connected"))
+.catch(err=>console.log("Mongo Connection error"))
+
+const PORT = 3000
+app.listen(PORT,()=>{
+    console.log("Server running on port "+PORT+"...")
+})
